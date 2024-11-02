27e1c5366a31c251da26fda25848d918cc86f904
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-02 23:59:33 +0600

    added component

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["assets.acme.com"],
+  },
+};
+
+module.exports = nextConfig;
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 const GalleryPage = () => {
   return (
     <div>
       <h1 className="text-4xl text-center">Imahe optimizations</h1>
       <h2 className="text-center text-2xl">Regular Image Tag</h2>
-      <img
+
+      <Image
         src="https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg"
         alt="next-img"
         className="mx-auto"
