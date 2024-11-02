cc90713091cb6f021e2788494bef1c19b7ff1eef
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-02 23:49:31 +0600

    added image component

---
+++ src/app/gallery/page.js
@@ -0,0 +1,15 @@
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center">Imahe optimizations</h1>
+      <h2 className="text-center text-2xl">Regular Image Tag</h2>
+      <img
+        src="https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg"
+        alt="next-img"
+        className="mx-auto"
+      />
+    </div>
+  );
+};
+
+export default GalleryPage;
