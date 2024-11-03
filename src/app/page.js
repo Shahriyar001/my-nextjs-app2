7d992737ca411504f50d341822b7db5b82387241
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-03 22:53:07 +0600

    Set-up Json Server for Next Level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,32 @@ export const metadata = {
   description: "This is home Page",
 };
 
-const HomePage = () => {
+const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/shoes");
+  const shoes = await res.json();
+
   return (
-    <div>
+    <div className="mx-auto">
       <h1 className="text-4xl text-center">Welcome to mext.js </h1>
-      <Counter />
+      <div className="flex p-10 gap-6 justify-between flex-wrap">
+        {shoes?.slice(0, 3).map((shoe) => (
+          <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
