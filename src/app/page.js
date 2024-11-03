547bc1e5cda33ee750d410ba1d81bfe90c3a333f
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-03 23:49:33 +0600

    added caching method

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,9 @@ export const metadata = {
 };
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/shoes");
+  const res = await fetch("http://localhost:5000/shoes", {
+    cache: "forse-cache",
+  });
   const shoes = await res.json();
 
   return (
